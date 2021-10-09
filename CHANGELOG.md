d7e3b77bbac4b4a9d61dcd7a9c47d74acc927831
tsutsu3 <tsutsu3prog@gmail.com>
2021-10-09 17:28:08 +0900

    Release v1.0.2 (#27)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## v1.0.2 (2021-10-09)
+
+- Fix: Schema key containing - not producing matches (#26)
+
 ## v1.0.1 (2020-12-18)
 
 ### Added
--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, tsutsu3"
 author = "tsutsu3"
 
 # The full version, including alpha/beta/rc tags
-release = "v1.0.0"
+release = "v1.0.2"
 
 
 # -- General configuration ---------------------------------------------------
