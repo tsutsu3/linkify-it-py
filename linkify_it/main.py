f5143a277470eb2f1e2fb7339bfbded99c8a5a94
tsutsu3 <tsutsu3prog@gmail.com>
2022-05-06 17:28:58 +0900

    Add match_at_start method

--- linkify_it/main.py
+++ linkify_it/main.py
@@ -361,6 +361,7 @@ class LinkifyIt:
         # (?!_) cause 1.5x slowdown
         self.re["schema_test"] = re_schema_test
         self.re["schema_search"] = re_schema_test
+        self.re["schema_at_start"] = "^" + self.re["schema_search"]
 
         self.re["pretest"] = (
             "(" + re_schema_test + ")|(" + self.re["host_fuzzy_test"] + ")|@"
@@ -558,6 +559,38 @@ class LinkifyIt:
             return result
 
         return None
+
+    def match_at_start(self, text):
+        """Returns fully-formed (not fuzzy) link if it starts at the beginning
+        of the string, and null otherwise.
+
+        Args:
+            text (str): text to search
+
+        Retuns:
+            ``Match`` or ``None``
+        """
+        # Reset scan cache
+        self._text_cache = text
+        self._index = -1
+
+        if not len(text):
+            return None
+
+        founds = re.search(self.re["schema_at_start"], text, flags=re.IGNORECASE)
+        if not founds:
+            return None
+
+        m = (founds.group(), founds.groups()[0], founds.groups()[1])
+        length = self.test_schema_at(text, m[2], len(m[0]))
+        if not length:
+            return None
+
+        self._schema = m[2]
+        self._index = founds.start(0) + len(m[1])
+        self._last_index = founds.start(0) + len(m[0]) + length
+
+        return self._create_match(0)
 
     def tlds(self, list_tlds, keep_old=False):
         """Load (or merge) new tlds list. (chainable)
